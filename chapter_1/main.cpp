b71cc8dc22fd058cc10ad4c5bbdf2eeca21c6aca
janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2020-08-08 12:58:54 +0200

    employee database works

--- chapter_1/main.cpp
+++ chapter_1/main.cpp
@@ -1,21 +1,134 @@
 #include <iostream>
-#include "Employee.h"
+#include <stdexcept>
+#include <exception>
+#include "Database.h"
 
 using namespace std;
 using namespace Rec;
 
+int displayMenu();
+void doHire(Database& db);
+void doFire(Database& db);
+void doPromote(Database& db);
+void doDemote(Database& db);
+
+
 int main(int args, char* argv)
 {
-    cout << "EMPLOYEE CLASS TEST" << endl;
-    Employee emp;
-    emp.setFirstName("John");
-    emp.setLastName("Doe");
-    emp.setEmployeeNumber(71);
-    emp.setSalary(50000);
-    emp.promote();
-    emp.promote(50);
-    emp.hire();
-    emp.display();
-
+    Database employeeDB;
+    bool done = false;
+    while (!done) {
+        int selection = displayMenu();
+        switch (selection) {
+        case 0:
+            done = true;
+            break;
+        case 1:
+            doHire(employeeDB);
+            break;
+        case 2:
+            doFire(employeeDB);
+            break;
+        case 3:
+            doPromote(employeeDB);
+            break;
+        case 4:
+            employeeDB.displayAll();
+            break;
+        case 5:
+            employeeDB.displayCurrent();
+            break;
+        case 6:
+            employeeDB.displayFormer();
+            break;
+        default:
+            cerr << "Unknown Command. " << endl;
+            break;
+        }
+    }
     return 0;
 }
+
+
+int displayMenu() {
+    int selection;
+    cout << endl;
+    cout << "Employee Database" << endl;
+    cout << "-----------------" << endl;
+    cout << "1) Hire new Employee" << endl;
+    cout << "2) Fire an Employee" << endl;
+    cout << "3) Promote an Employee" << endl;
+    cout << "4) List all Employees" << endl;
+    cout << "5) List all current Employees" << endl;
+    cout << "6) List all former Employees" << endl;
+    cout << "0) Quit" << endl;
+    cout << endl;
+    cout << "---> ";
+    cin >> selection;
+    return selection;
+}
+
+void doHire(Database& db) {
+    string firstName;
+    string lastName;
+    cout << "First Name? ";
+    cin >> firstName;
+
+    cout << "Last Name?";
+    cin >> lastName;
+
+    db.addEmployee(firstName, lastName);
+}
+
+void doFire(Database& db) {
+    int employeeNumber;
+
+    cout << "Employee number?";
+    cin >> employeeNumber;
+
+    try {
+        Employee& emp = db.getEmployee(employeeNumber);
+        emp.fire();
+        cout << "Employee " << employeeNumber << " terminated" << endl;
+    }
+    catch (const std::logic_error& e) {
+        cerr << "Unable to terminate employee: " << e.what() << endl;
+    }
+}
+void doPromote(Database& db) {
+    int employeeNumber;
+    int raiseAmount;
+
+    cout << "Employee number? ";
+    cin >> employeeNumber;
+
+    cout << "How much of a raise? ";
+    cin >> raiseAmount;
+
+    try {
+        Employee &emp = db.getEmployee(employeeNumber);
+        emp.promote(raiseAmount);
+    }
+    catch (const std::logic_error& e) {
+        cerr << "Unable to promote employee: " << e.what() << endl;
+    }
+}
+void doDemote(Database& db) {
+    int employeeNumber;
+    int demoteAmount;
+
+    cout << "Employee number? ";
+    cin >> employeeNumber;
+
+    cout << "How much of a raise? ";
+    cin >> demoteAmount;
+
+    try {
+        Employee &emp = db.getEmployee(employeeNumber);
+        emp.demote(demoteAmount);
+    }
+    catch (const std::logic_error& e) {
+        cerr << "Unable to demote employee: " << e.what() << endl;
+    }
+}
+
